a3f10908699d5d01943f4a619eb3c08acdaa3f01
ghobadh <ghobadh@gmail.com>
2024-09-30 21:52:08 -0400

    Initial commit for Gargamel Pet Clinic project - Spring Boot

---
+++ src/main/java/ca/gforcesoftware/gargamelpetclinic/GargamelPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.gargamelpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GargamelPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GargamelPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gforcesoftware/gargamelpetclinic/GargamelPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.gargamelpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GargamelPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
