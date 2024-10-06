74c70c9dc3b41de17debd829f5eabc46bb5a2192
ghobadh <ghobadh@gmail.com>
2024-10-05 21:18:52 -0400

    Index controller and template index.html are added. close #8

---
+++ pet-clinic-web/src/main/java/ca/gforcesoftware/gargamelclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package ca.gforcesoftware.gargamelclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author gavinhashemi on 2024-10-05
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/","","index","index.html"})
+    public String index() {
+        return "index";
+    }
+}
