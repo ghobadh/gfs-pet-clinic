8a3621752104bafc836fd96c803b01d866914396
ghobadh <ghobadh@gmail.com>
2024-10-09 21:28:28 -0400

    added Prt Type Service are added. close #29

---
+++ pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package ca.gforcesoftware.gargamelclinic.services;
+
+import ca.gforcesoftware.gargamelclinic.model.PetType;
+
+/**
+ * @author gavinhashemi on 2024-10-09
+ */
+public interface PetTypeService extends CrudService<PetType, Long>  {
+}
---
+++ pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package ca.gforcesoftware.gargamelclinic.services.map;
+
+import ca.gforcesoftware.gargamelclinic.model.PetType;
+import ca.gforcesoftware.gargamelclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author gavinhashemi on 2024-10-09
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService< PetType,  Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+            super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType t) {
+        super.delete(t);
+    }
+
+    @Override
+    public PetType save(PetType t) {
+        return super.save(t);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
