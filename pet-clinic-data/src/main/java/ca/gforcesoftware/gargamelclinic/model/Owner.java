6aceee40d0b2daeb9a174e3037c8c574233c49b1
ghobadh <ghobadh@gmail.com>
2024-10-09 21:12:32 -0400

    Contact are added to Owners. close #28

--- pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/model/Owner.java
@@ -6,6 +6,9 @@ import java.util.Set;
  * @author gavinhashemi on 2024-10-04
  */
 public class Owner extends Person{
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
     public Set<Pet> getPets() {
@@ -14,5 +17,29 @@ public class Owner extends Person{
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
