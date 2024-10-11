3338c84377dfc434456a940460bed3d80b10b88e
ghobadh <ghobadh@gmail.com>
2024-10-11 19:41:22 -0400

    @MappedSupperclass is added to BaseEntity. close #35

--- pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/model/BaseEntity.java
@@ -1,11 +1,19 @@
 package ca.gforcesoftware.gargamelclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
 /**
  * @author gavinhashemi on 2024-10-05
  */
+@MappedSuperclass
 public class BaseEntity  implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
