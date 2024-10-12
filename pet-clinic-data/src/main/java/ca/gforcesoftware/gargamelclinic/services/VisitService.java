b3346ff8adf7a516a2da5c88288d60e0835e8500
ghobadh <ghobadh@gmail.com>
2024-10-12 10:35:24 -0400

    VisitMapService and VisitService are implemented.  close #46

---
+++ pet-clinic-data/src/main/java/ca/gforcesoftware/gargamelclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package ca.gforcesoftware.gargamelclinic.services;
+
+import ca.gforcesoftware.gargamelclinic.model.Visit;
+
+/**
+ * @author gavinhashemi on 2024-10-12
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
