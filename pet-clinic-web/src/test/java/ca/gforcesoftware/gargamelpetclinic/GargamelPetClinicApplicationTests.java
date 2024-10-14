fb41b5ef8ca0b5ebbc130f7f5456664fb1d7f3c4
ghobadh <ghobadh@gmail.com>
2024-10-14 09:37:56 -0400

    I added surefire plugin in pom file but I am not sure it was really changed anything as some of pluging such as surefire-provider is completely deprecated

--- pet-clinic-web/src/test/java/ca/gforcesoftware/gargamelpetclinic/GargamelPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/ca/gforcesoftware/gargamelpetclinic/GargamelPetClinicApplicationTests.java
@@ -1,13 +1,17 @@
 package ca.gforcesoftware.gargamelpetclinic;
 
+import ca.gforcesoftware.gargamelclinic.GargamelPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
+@SpringBootTest(classes = GargamelPetClinicApplication.class)
 class GargamelPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
